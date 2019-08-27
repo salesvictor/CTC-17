e6e56f05f680070ff7153b723bdab9ce7a8cfa8b
Victor Sales <victor@vsales.io>
2019-08-27 14:14:20 -0300

    Updated greedy()

--- Projeto 1/Core.py
+++ Projeto 1/Core.py
@@ -2,28 +2,36 @@ import numpy as np
 import heapq as hp
 
 class Node:
-	def __init__(self, parent, depth, state, cost):
-		self.parent = parent
-		self.depth = depth
-		self.state = state
-		self.cost = cost
-		self.children = []
+    def __init__(self, parent, depth, state, cost):
+        self.parent = parent
+        self.depth = depth
+        self.state = state
+        self.cost = cost
+        self.children = []
 
-	def __lt__(self, other):
-		return self.cost < other.cost
+        def __lt__(self, other):
+            return self.cost < other.cost
 
-	def createSon(self, state, cost):
-		child = Node(self, self.depth + 1, state, cost)
-		hp.heappush(self.children, child)
+        def createSon(self, state, cost):
+            child = Node(self, self.depth + 1, state, cost)
+            hp.heappush(self.children, child)
 
 def greedy(node, MainClass, actions):
-	for action in actions:
-		node.createSon(action, MainClass.cost(action))
-	if node.state == REF:
-		return True
-	else:
-		finished = False
-	while(not finished and len(node.children)):
-		finished = greedy(hp.heappop(node.children), MainClass, MainClass.getActions(node.state))
+    # Check if in the final state
+    if node.state == MainClass.goal_state:
+        return node
 
+    # If not, check children
+    for action in actions:
+        node.createSon(action, MainClass.cost(action))
 
+    while node.children:
+        leaf_node = greedy(hp.heappop(node.children), MainClass, MainClass.getActions(node.state))
+        if leaf_node == None:
+            continue
+        elif leaf_node.state == MainClass.goal_state:
+            return leaf_node
+        elif not leaf_node.children:
+            return None
+
+    return None
