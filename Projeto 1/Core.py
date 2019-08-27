9559116bb62897f4a00ee44c71da3384dfef8fa1
Eduardo Alarcon <eduambarbosa@gmail.com>
2019-08-27 00:01:07 -0300

    Problema 1 init

--- Projeto 1/Core.py
+++ Projeto 1/Core.py
@@ -1,14 +1,13 @@
 class Node:
-	def __init__(self, father, depth, state):
+	def __init__(self, father, depth, state, cost):
 		self.father = father
 		self.depth = depth
 		self.state = state
-		self.sons = []
-		self.cost = None
-
-	def setCost(self, cost):
 		self.cost = cost
+		self.sons = []
 
-	def createSon(self, newSon):
-		self.sons.append(newSon)
+	def createSon(self, state, cost):
+		son = Node(self, self.depth + 1, state, cost)
+		self.sons.append(son)
+		son.father = self
 
--- Projeto 1/Problema1.py
+++ Projeto 1/Problema1.py
@@ -1,11 +1,13 @@
 import csv
 import Core.py as Core
+from math import *
 
 Cities = []
+Graph = [][]
 
 class City:
-	def __init__(self, id, name, lat, lng):
-		self.id = id
+	def __init__(self, cityid, name, lat, lng):
+		self.cityid = cityid
 		self.name = name
 		self.lat = lat
 		self.lng = lng
@@ -13,20 +15,39 @@ class City:
 def readMap():
 	with open('australia.csv') as csvfile:
 	    readCSV = csv.reader(csvfile, delimiter=',')
+	    Cities.append(City(readCSV[1][0], readCSV[1][1], readCSV[1][2], readCSV[1][3]))
 	    for row in readCSV[1:]:
-	    	Cities[row[0]] = City(row[0], row[1], row[2], row[3])
+	    	Cities.append(City(row[0], row[1], row[2], row[3]))
 
-def getNextCities(City):
-	
-def getMinimum(CityA, CityB):
+def getDistance(city1, city2):
+	distance = sqrt(pow(city1.lat - city2.lat,2) + pow(city1.lng - city2.lng,2))
+	return distance
+
+def createGraph():
+	for city in Cities:
+		if city.cityid > 1 and city.cityid%2 == 0:
+			if city.cityid + 2 <= len(Cities):
+				Graph.append([city.cityid, city.cityid + 2, getDistance(city, Cities[city.cityid + 2])])
+			Graph.append([city.cityid, city.cityid - 1, getDistance(city, Cities[city.cityid - 1])])
+
+		else if city.cityid%2 == 1 and city.cityid > 2:
+			if city.cityid + 1 <= len(Cities):
+				Graph.append([city.cityid, city.cityid + 1, getDistance(city, Cities[city.cityid + 1])])
+			Graph.append([city.cityid, city.cityid - 2, getDistance(city, Cities[city.cityid - 2])])
+
+def getMinimum(curCity, endCity):
 
 
 def main():
 	readMap()
 
-	AliceSprings = City(5)
-	Yulara = City(219)
-	getMinimum(CityA, CityB)
+	startCity = Cities[5]
+	endCity = Cities[219]
+
+	createGraph()
+
+	root = Node(None, 0, [startCity.cityid], 0)
+	getMinimum(startCity, endCity)
 
 	
 
