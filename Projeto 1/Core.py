4d85a27c1d5e29eb23fb933e7f971688c19a6145
Victor Sales <victor@vsales.io>
2019-08-27 16:17:03 -0300

    Some things

--- Projeto 1/Core.py
+++ Projeto 1/Core.py
@@ -8,27 +8,29 @@ class Node:
         self.cost = cost
         self.children = []
 
-        def __lt__(self, other):
-            return self.cost < other.cost
+    def __lt__(self, other):
+        return self.cost < other.cost
 
-        def createSon(self, state, cost):
-            child = Node(self, self.depth + 1, state, cost)
-            hp.heappush(self.children, child)
+    def createChild(self, state, cost):
+        child = Node(self, self.depth + 1, state, cost)
+        hp.heappush(self.children, child)
 
-def greedy(node, MainClass, actions):
+def greedy(node, MainClass):
+    print(f'depth: {node.depth}\ncost:{node.cost}\nstate:\n{MainClass.construct_by_state(node.state[-1])}')
     # Check if in the final state
-    if node.state == MainClass.goal_state:
+    if MainClass.is_goal(node.state):
         return node
 
     # If not, check children
+    actions = MainClass.getActions(node.state)
     for action in actions:
-        node.createSon(action, MainClass.cost(action))
+        node.createChild(action, MainClass.cost(action))
 
     while node.children:
-        leaf_node = greedy(hp.heappop(node.children), MainClass, MainClass.getActions(node.state))
+        leaf_node = greedy(hp.heappop(node.children), MainClass)
         if leaf_node == None:
             continue
-        elif leaf_node.state == MainClass.goal_state:
+        elif MainClass.is_goal(leaf_node.state):
             return leaf_node
         elif not leaf_node.children:
             return None
