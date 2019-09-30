69473f88564b9e4f917bce7fed328c8348d6c10a
Victor Sales <victor@vsales.io>
2019-09-30 16:23:43 -0300

    Implemented pseudo ID3

--- Projeto 3/core.py
+++ Projeto 3/core.py
@@ -1,3 +1,6 @@
+import math
+
+
 class Node:
     def __init__(self):
         self.label = None
@@ -14,7 +17,7 @@ class Tree:
 
 
 def parse_file(filename: str):
-    file = open(filename, 'r')
+    file = open(filename, 'r', encoding='latin-1')
     lines = file.readlines()
     vals = []
     for line in lines:
@@ -22,6 +25,42 @@ def parse_file(filename: str):
 
     file.close()
     return vals
+
+
+def value_counter(values):
+    value_count = {x: 0 for x in values}
+    for value in values:
+        value_counter[value] += 1
+
+    return value_count
+
+
+def entropy(outcomes):
+    entropy = 0
+    total = len(outcomes)
+    outcome_count = value_counter(outcomes)
+    for count in outcome_count.values():
+        entropy -= count/total * math.log2(count/total)
+
+    return entropy
+    
+
+def information_gain(examples, target_attribute, attribute):
+    attribute_values = set()
+    for value in examples[attribute]:
+        attribute_values.add(value)
+
+    information_entropy = 0
+    example_outcomes = examples[target_attribute]
+    total = len(example_outcomes)
+    for attribute_value in attribute_values:
+        outcomes = examples[attribute == attribute_value][target_attribute]
+        value_entropy = entropy(outcomes)
+        outcome_count = value_counter(outcomes)
+        for count in outcome_count.values():
+            information_entropy += count/total * value_entropy
+
+    return entropy(example_outcomes) - information_entropy
 
 
 def most_common(examples):
