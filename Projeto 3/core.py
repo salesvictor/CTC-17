8e75aba16c6abed0569358748ee51e80abd512d6
Eduardo Alarcon <eduambarbosa@gmail.com>
2019-10-01 00:56:41 -0300

    Final code

--- Projeto 3/core.py
+++ Projeto 3/core.py
@@ -1,5 +1,6 @@
 import math
 import copy
+import pandas as pd
 
 class Node:
     def __init__(self):
@@ -29,7 +30,7 @@ def parse_file(filename: str):
 def value_counter(values: list) -> dict:
     value_count = {x: 0 for x in values}
     for value in values:
-        value_counter[value] += 1
+        value_count[value] += 1
 
     return value_count
 
@@ -45,21 +46,34 @@ def entropy(outcomes: list) -> float:
     
 
 def information_gain(examples, entire_entropy) -> float:
+    print("Information gain")
 
     if type(examples.iloc[0,0]) == list:
-        
+        attribute_values = {item for sublist in examples.iloc[:,0] for item in sublist}
+
+        information_entropy = 0
+        total = len(examples.index)
+        header = list(examples.columns)
+        for attribute_value in attribute_values:
+            mask = examples.iloc[:,0].apply(lambda x: attribute_value in x)
+            outcomes = examples[mask].iloc[:,-1]
+            value_entropy = entropy(outcomes)
+            outcome_count = value_counter(outcomes)
+            for count in outcome_count.values():
+                information_entropy += count/total * value_entropy
+
     else:
         attribute_values = set(examples.iloc[:,0])
 
-    information_entropy = 0
-    total = len(examples.index)
-    header = list(examples.columns)
-    for attribute_value in attribute_values:
-        outcomes = (examples[examples[header[0]] == attribute_value]).iloc[:,-1]
-        value_entropy = entropy(outcomes)
-        outcome_count = value_counter(outcomes)
-        for count in outcome_count.values():
-            information_entropy += count/total * value_entropy
+        information_entropy = 0
+        total = len(examples.index)
+        header = list(examples.columns)
+        for attribute_value in attribute_values:
+            outcomes = (examples[examples[header[0]] == attribute_value]).iloc[:,-1]
+            value_entropy = entropy(outcomes)
+            outcome_count = value_counter(outcomes)
+            for count in outcome_count.values():
+                information_entropy += count/total * value_entropy
 
     return entire_entropy - information_entropy
 
@@ -68,12 +82,12 @@ def most_common(values: list):
     return max(set(values), key=values.count)
 
 def best_attribute(examples):
-    entire_entropy = entropy(examples.loc[:,-1])
+    entire_entropy = entropy(examples.iloc[:,-1])
 
     attributes_entropy = []
 
     for i in range(len(examples.columns)):
-        attributes_entropy.append(information_gain(examples.loc[:,[i,-1]], entire_entropy))
+        attributes_entropy.append(information_gain(examples.iloc[:,[i,-1]], entire_entropy))
 
     header = list(examples.columns)
 
@@ -82,12 +96,14 @@ def best_attribute(examples):
 
 def decision_tree(examples):
 
+    print("Decision Tree")
+
     tree = Tree()
 
-    if len(set(examples.loc[:,-1])) == 1:
-        tree.root.label = examples.loc[0,-1]
-    elif not attributes:
-        tree.root.label = most_common(examples.loc[:,-1])
+    if len(set(examples.iloc[:,-1])) == 1:
+        tree.root.label = examples.iloc[0,-1]
+    elif len(examples.columns) == 1:
+        tree.root.label = most_common(examples.iloc[:,-1])
     else:
         attribute = best_attribute(examples)
         tree.root.attribute = attribute
@@ -95,13 +111,18 @@ def decision_tree(examples):
             child_tree = Tree()
             relevant_examples = examples[examples[attribute] == value]
             if not relevant_examples:
-                child_tree.root.label = most_common(examples.loc[:,-1])
+                child_tree.root.label = most_common(examples.iloc[:,-1])
             else:
                 child_tree = decision_tree(relevant_examples.drop(columns=[attribute]))
 
             tree.add_child(child_tree)
 
     return tree
+
+    def print_tree(tree):
+        print("(Label: ",tree.root.label, " , Attribute: ", tree.root.attribute, ")")
+        for child in tree.children:
+            print_tree(child)
 
 if __name__ == "__main__":
 
@@ -114,3 +135,6 @@ if __name__ == "__main__":
 
     final = b.loc[:,['Gender', 'Age', 'Occupation', 'Genres', 'Rating']]
     final['Genres'] = final['Genres'].str.split(pat="|")
+
+    answer = decision_tree(final)
+    print_tree(answer)
